7819afc47f4d577aee8c8de5a102eb8beffec0a7
Omar Hossam <moar.ahmed@gmail.com>
2018-12-12 20:59:45 +0200

    latest

---
+++ src/analysis_assignment_2/GradingVisitor.java
@@ -0,0 +1,14 @@
+package analysis_assignment_2;
+
+public class GradingVisitor implements Visitor {
+    protected String _strResult = new String();
+    public void visit( Vertex v ){
+        _strResult += "v=" + v.getUniqueID( ) + " ";
+    }
+    public void visit( Edge e ){
+    _strResult += "e=" + e.getUniqueID( ) + " " ;
+}
+    public String getResult( ){
+        return _strResult;
+    }
+}
--- src/analysis_assignment_2/Graph.java
+++ src/analysis_assignment_2/Graph.java
@@ -1,14 +1,16 @@
 package analysis_assignment_2;
 
-import java.util.Vector;
+import java.util.*;
 
 public class Graph {
 	public Vector<Vertex> vertices;
 	public Vector<PathSegment> paths;
+	public Vector<Edge> edges;
 	
 	public Graph() {
 		vertices = new Vector<Vertex>();
 		paths = new Vector<PathSegment>();
+		edges = new Vector<Edge>();
 	}
 	
 	//  returns the name you have given to this graph library [1 pt]
@@ -23,10 +25,12 @@ public class Graph {
 	}
 	
 	// the following method adds a vertex to the graph [2 pts]
-	 public void insertVertex(String strUniqueID, String strData, int nX, int nY) {
+	 public void insertVertex(String strUniqueID, String strData, int nX, int nY) throws GraphException {
 		 if(getVertexByUniqueID(strUniqueID) == null)
 		 	vertices.add(new Vertex(strUniqueID, strData, nX, nY));
-	 };
+		 else
+		 	throw new GraphException("Vertex exists!");
+	 }
 	
 	// inserts an edge between 2 specified vertices [2 pts]
 	public void insertEdge(String strVertex1UniqueID, String strVertex2UniqueID, 
@@ -38,6 +42,7 @@ public class Graph {
 			throw new GraphException("One of the vertices does not exist");
 		paths.add(new PathSegment(vertex1, edge));
 		paths.add(new PathSegment(vertex2, edge));
+		edges.add(edge);
 	}
 
 	// removes vertex and its incident edges [1 pt]
@@ -62,7 +67,7 @@ public class Graph {
 	// removes an edge from the graph [1 pt]
 	public void removeEdge(String strEdgeUniqueID) throws GraphException {
 		for(int i=0; i < paths.size(); i++) {
-			if(paths.get(i).getEdge().getUniqueID().equals(new StringBuffer(strEdgeUniqueID)))
+			if(paths.get(i).getEdge().getUniqueID().toString().equals(strEdgeUniqueID))
 				paths.remove(i);
 		}
 	}
@@ -72,7 +77,7 @@ public class Graph {
 	public Vector<Edge> incidentEdges(String strVertexUniqueID) throws GraphException{
 		Vector<Edge> edges = new Vector<Edge>();
 		for(PathSegment path : paths)
-			if(path.getVertex().getUniqueID().equals(new StringBuffer(strVertexUniqueID)))
+			if(path.getVertex().getUniqueID().toString().equals(strVertexUniqueID))
 				edges.add(path.getEdge());
 		return edges;
 	}
@@ -85,31 +90,93 @@ public class Graph {
 	}
 
 	// returns all edges in the graph [1 pt]
-	//	public Vector<Edge> edges() throws GraphException
+		public Vector<Edge> edges() throws GraphException {
+			return edges;
+		}
 
 	// returns an array of the two end vertices of the passed edge [1 pt]
-	//	public Vertex[] endVertices(String strEdgeUniqueID) throws GraphException
+		public Vertex[] endVertices(String strEdgeUniqueID) throws GraphException {
+			ArrayList<Vertex> v = new ArrayList<Vertex>();
+			for (PathSegment path : paths) {
+				if(path.getEdge().getUniqueID().toString().equals(strEdgeUniqueID))
+					v.add(path.getVertex());
+			}
+			return  new Vertex[]{v.get(0), v.get(1)};
+		}
 	
 	// returns the vertex opposite of another vertex [1 pt]
-	//	public Vertex opposite(String strVertexUniqueID, String strEdgeUniqueID) throws GraphException
+		public Vertex opposite(String strVertexUniqueID, String strEdgeUniqueID) throws GraphException {
+			Vertex[] v = endVertices(strEdgeUniqueID);
+			if (v[0].getUniqueID().toString().equals(strVertexUniqueID))
+				return v[1];
+			else
+				return v[0];
+		}
 
 	// performs depth first search starting from passed vertex visitor is called on each vertex and edge visited. [12 pts]
-	//	public void dfs(String strStartVertexUniqueID, Visitor visitor) throws GraphException
+		public void dfs(String strStartVertexUniqueID, Visitor visitor) throws GraphException {
+			Stack<String> toBeVisited = new Stack<String>();
+			toBeVisited.add(strStartVertexUniqueID);
+			HashSet<String> visited = new HashSet<>();
+
+			while (!toBeVisited.isEmpty()) {
+				String nextVertex = toBeVisited.pop();
+				if (visited.contains(nextVertex))
+					continue;
+
+				visitor.visit(getVertexByUniqueID(nextVertex));
+				visited.add(nextVertex);
+
+				Vector<Edge> incidentEdges = incidentEdges(nextVertex);
+
+				for (Edge e : incidentEdges) {
+					visitor.visit(e);
+					Vertex v = opposite(nextVertex,e._strUniqueID.toString());
+					if (!visited.contains(v._strUniqueID.toString()))
+						toBeVisited.push(v._strUniqueID.toString());
+				}
+			}
+		}
 
 
 	 // performs breadth first search starting from passed vertex visitor is called on each vertex and edge visited. [17 pts]
-	//	public void bfs(String strStartVertexUniqueID, Visitor visitor) throws GraphException
+		public void bfs(String strStartVertexUniqueID, Visitor visitor) throws GraphException {
+			HashSet<String> visited = new HashSet<>();
+			LinkedList<String> queue = new LinkedList<>();
+			queue.addLast(strStartVertexUniqueID);
+
+			while (!queue.isEmpty()) {
+				String nextVertex = queue.pollFirst();
+
+				if (visited.contains(nextVertex))
+					continue;
+
+				visitor.visit(getVertexByUniqueID(nextVertex));
+				visited.add(nextVertex);
+
+				Vector<Edge> incidentEdges = incidentEdges(nextVertex);
+
+				for (Edge e : incidentEdges) {
+					visitor.visit(e);
+					Vertex v = opposite(nextVertex,e._strUniqueID.toString());
+					if (!visited.contains(v._strUniqueID.toString()))
+						queue.addLast(v._strUniqueID.toString());
+				}
+			}
+		}
 	
 	// returns a path between start vertex and end vertex if exists using dfs. [18 pts]
 	//	public Vector<PathSegment> pathDFS( String strStartVertexUniqueID, String strEndVertexUniqueID) throws GraphException
 	
 	// finds the closest pair of vertices using divide and conquer algorithm. Use X and Y attributes in each vertex. [30 pts]
-	//	public Vertex[] closestPair() throws GraphException 
+		public Vertex[] closestPair() throws GraphException {
+
+		}
 	
 	/*			HELPER FUNCTIONS 			*/
 	public Vertex getVertexByUniqueID(String uniqueId) {
 		for(Vertex vertex : vertices)
-			if(vertex.getUniqueID().equals(new StringBuffer(uniqueId)))
+			if(vertex.getUniqueID().toString().equals(uniqueId))
 				return vertex;
 		return null;
 	}
@@ -120,6 +187,10 @@ public class Graph {
 				return path.getEdge();
 		}
 		return null;
+	}
+
+	public static void main(String [] args) {
+
 	}
 
 }
